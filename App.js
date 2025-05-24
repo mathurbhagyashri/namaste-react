2d3e7870f91dacef92015dd73bf1a5bc2acec17d
Bhagyashri <mathurbhagyashri@gmail.com>
2025-05-24 18:40:13 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+/* <div id="parent">
+<div id="child">
+* <h1>I am H1 tag</h1> 
+ <h2>I am H2 tag</h2> 
+ </div>
+ </div> */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "I am h1 tag "),
+    React.createElement("h2", { id: "heading" }, "h2 tag "),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "newText" }, "i am child2"),
+    React.createElement("h1", { id: "newText" }, "i am child2's sibling"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "learning React episode 1"
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
