09210c3a9cf3b52de9e4f07efbd35ef83f21679b
Bhagyashri <mathurbhagyashri@gmail.com>
2025-07-30 20:03:05 +0530

    part 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,68 @@
 import { RestaurantCard } from "./RestaurantCard";
-import { useState } from "react";
-import resLists from '../utils/mockData'
+import { useEffect, useState } from "react";
+import resLists from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 export const Body = () => {
+  console.log("searender");
+
+  // local state variable - super powerful variable to create state
   const [listsOfRestaurants, setListsOfRestaurants] = useState(resLists);
+   const [serchRestaurantsList, setSerchRestaurantsList] = useState(resLists);
+
+  const [searchtext, setSearchText] = useState("");
+
+  useEffect(() => {
+    // fetchData();
+    setListsOfRestaurants(resLists);
+    setSerchRestaurantsList(resLists)
+  }, []);
+
+  // const fetchData = async () => {
+  //   const data = await fetch(
+  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  //   );
+
+  //   const json = await data.json();
+  //   console.warn("json data api", json);
+
+  //   // console.warn(json.data.cards[9].card);
+
+  //   // setListsOfRestaurants(json?.data?.cards[0]? .card.card.imageGridCards.info)
+  //   // console.log("atom",json.data.cards[0].card.card.imageGridCards.info);
+  // };
+
+  // console.log("listsOfRestaurants", listsOfRestaurants);
+
+  if (listsOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            className="search-box"
+            type="text"
+            value={searchtext}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // filter the restaurants card and updtae the ui
+              // searchtext
+              console.log("searchtext", searchtext);
+              const filteredResList = listsOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchtext.toLowerCase())
+              );
+              setSerchRestaurantsList(filteredResList);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/contants";
+
+export const Header = () => {
+const [btnName,setbtnName]=useState('Login');
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={LOGO_URL}
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+       <button onClick={()=>{btnName==="Login"? setbtnName('Logout'):setbtnName('Login')}} className="login-btn">{btnName}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
