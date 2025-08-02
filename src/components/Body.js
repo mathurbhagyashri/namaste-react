0f52f7d7f18467e1961928d8c638662a84a9b947
Bhagyashri <mathurbhagyashri@gmail.com>
2025-08-03 01:13:51 +0530

    part 6 fixed swiggy api issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,38 +1,35 @@
-import { RestaurantCard } from "./RestaurantCard";
+import  RestaurantCard  from "./RestaurantCard";
 import { useEffect, useState } from "react";
-import resLists from "../utils/mockData";
+// import resLists from "../utils/mockData";
 import Shimmer from "./Shimmer";
 
 export const Body = () => {
   console.log("searender");
 
   // local state variable - super powerful variable to create state
-  const [listsOfRestaurants, setListsOfRestaurants] = useState(resLists);
-   const [serchRestaurantsList, setSerchRestaurantsList] = useState(resLists);
+  const [listsOfRestaurants, setListsOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
   const [searchtext, setSearchText] = useState("");
 
   useEffect(() => {
-    // fetchData();
-    setListsOfRestaurants(resLists);
-    setSerchRestaurantsList(resLists)
+    fetchData();
   }, []);
 
-  // const fetchData = async () => {
-  //   const data = await fetch(
-  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  //   );
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-  //   const json = await data.json();
-  //   console.warn("json data api", json);
+    const json = await data.json();
+    console.warn("json data api", json);
 
-  //   // console.warn(json.data.cards[9].card);
+        console.warn("ready", json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+);
 
-  //   // setListsOfRestaurants(json?.data?.cards[0]? .card.card.imageGridCards.info)
-  //   // console.log("atom",json.data.cards[0].card.card.imageGridCards.info);
-  // };
-
-  // console.log("listsOfRestaurants", listsOfRestaurants);
+   setListsOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
 
   if (listsOfRestaurants.length === 0) {
     return <Shimmer />;
@@ -51,13 +48,13 @@ export const Body = () => {
           />
           <button
             onClick={() => {
-              // filter the restaurants card and updtae the ui
+              // filter the restaurants card and update the ui
               // searchtext
               console.log("searchtext", searchtext);
-              const filteredResList = listsOfRestaurants.filter((res) =>
-                res.data.name.toLowerCase().includes(searchtext.toLowerCase())
+              const filteredRestaurants = listsOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchtext.toLowerCase())
               );
-              setSerchRestaurantsList(filteredResList);
+              setFilteredRestaurant(filteredRestaurants);
             }}
           >
             search
@@ -67,18 +64,18 @@ export const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listsOfRestaurants.filter(
-              (res) => res.data.avgRating >= 4
+              (res) => res.info.avgRating >4
             );
-            console.log("listsOfRestaurants", listsOfRestaurants);
-            setListsOfRestaurants(filteredList);
+            console.log("filteredList89988", filteredList);
+           setFilteredRestaurant(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listsOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -4,7 +4,12 @@ const Shimmer=()=>{
     
 
 return <div className="shimmer-container">
-<div className="shimmer-card">cards</div>
+<div className="shimmer-card"></div>
+<div className="shimmer-card"></div>
+<div className="shimmer-card"></div>
+<div className="shimmer-card"></div>
+<div className="shimmer-card"></div>
+<div className="shimmer-card"></div>
 <div className="shimmer-card"></div>
 <div className="shimmer-card"></div>
 
