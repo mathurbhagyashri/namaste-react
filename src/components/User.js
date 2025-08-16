a9fe01735c0eda4f37d1475c6432c65e9fd2f7d0
Bhagyashri <mathurbhagyashri@gmail.com>
2025-08-16 17:24:42 +0530

    Episode 8 done

--- src/components/User.js
+++ src/components/User.js
@@ -3,19 +3,20 @@ import { useEffect, useState } from "react";
 const User = (props) => {
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(1);
-  const [userInfo,setUserInfo]= useState([])
+  const [userInfo, setUserInfo] = useState([]);
 
   useEffect(() => {
     // API Call
     getUser();
-   let timer= setInterval(() => {
+    let timer = setInterval(() => {
       console.log("React OP888");
     }, 1000);
 
     console.log("UseEffect");
 
+    // in functional component- in return will do the clean of code on moving out of the component
     return () => {
-      clearInterval(timer)
+      clearInterval(timer);
       console.log("UseEffect return");
     };
   }, []);
@@ -24,19 +25,21 @@ const User = (props) => {
   async function getUser() {
     const data = await fetch("https://api.github.com/users/mathurbhagyashri");
     const json = await data.json();
-    console.log("522",json);
-    setUserInfo(json)
+    console.log("522", json);
+    setUserInfo(json);
   }
 
-  const { name, location,avatar_url } = userInfo;
+  const { name, location, avatar_url } = userInfo;
   return (
     <div className="user-card">
       <div>
         <h1>Name: {name} </h1>
-      <h2>Location: {location}</h2>
-      <h3>contact: bhagyashri@gmail.com</h3>
+        <h2>Location: {location}</h2>
+        <h3>contact: bhagyashri@gmail.com</h3>
       </div>
-      <div ><img width='145' src={avatar_url} alt="UserImg"/></div>
+      <div>
+        <img width="145" src={avatar_url} alt="UserImg" />
+      </div>
       {/* <button onClick={() => setCount(count + 1)}>counter</button>
       <h4>count: {count}</h4>
       <h4>count2: {count2}</h4> */}
